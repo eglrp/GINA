9ac43ad05c73d4eb3460d9952ea7fef6911e4157
PINAProject <balazs.lupsic@gmail.com>
2018-02-20 13:23:33 +0100

    Commit the base of the IMU Simulator

---
+++ IMUSimulator/IMUSim/main.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "linalg.h"
+
+using namespace std;
+
+int main(int argc, char **argv){
+	alglib::boolean_1d_array b1;
+	b1 = "[true]";
+
+	return 0;
+}
