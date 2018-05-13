6b09ccb8a4db5a61447085a04166cb76610c85cc
PINAProject <balazs.lupsic@gmail.com>
2018-05-13 16:15:31 +0200

    Clean IMUSIMULATOR

---
+++ 60_IMUSimulator/include/CreateTrajectorywIMUGenerator.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#include "GPSWeekSecond.hpp"
+
+#include <Eigen/Dense>
+
+#include "strapdown_ecef.h"
+#include "geoparam_wgs84.h"
+#include "lib_Frames.h"
+
+#include "Trajectory.h"
+#include "IMUSignalGenerator.h"
+#include "IMUStore.h"
+
+//Own includes
+#include "Trajectory.h"
+#include "IMUSignalGenerator.h"
+#include "IMUStore.h"
+#include "IMUData.h"
+
+#include "..\68_PINAParser\include\TrajectoryParser\TrajectoryData.hpp"
+
+// PINAParser
+#include "IMUBase.hpp"
+#include "IMUData.hpp"
+#include "IMUHeader.hpp"
+#include "IMUStream.hpp"
+#include "IMUStore.hpp"
+
+#include "TrajectoryBase.hpp"
+#include "TrajectoryData.hpp"
+#include "TrajectoryHeader.hpp"
+#include "TrajectoryStream.hpp"
+#include "TrajectoryStore.hpp"
+
+
+void IMUGeneratorForTrajectory(std::string, std::string);
+
+void setPINAParsers(const PINASimulator::TrajectoryStream&,
+					PINASimulator::TrajectoryHeader&,
+					const PINASimulator::IMUStream&,
+					PINASimulator::IMUHeader&,
+					const Eigen::Vector3d&,
+					const Eigen::Vector3d&,
+					const unsigned char&, const double&,
+					const unsigned char&, const double&,
+					const double&);
+
+void generatetrajectory(IMUSimulator::IMUSignalGenerator&,
+						IMUSimulator::strapdown_ecef&,
+						IMUSimulator::PositionData&,
+						IMUSimulator::Measure_IMU&,
+						Eigen::Vector3d,
+						Eigen::Vector3d,
+						unsigned int,
+						double,
+						double);
+
+PINASimulator::TrajectoryData convert2PINAcompatible(IMUSimulator::PositionData&);
+PINASimulator::IMUData convert2PINAcompatible(IMUSimulator::Measure_IMU&);
