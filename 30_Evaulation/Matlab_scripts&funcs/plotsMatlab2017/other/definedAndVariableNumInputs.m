2a6fffce7204fae4a25a2f7ad2a771ea28c05353
PINAProject <balazs.lupsic@gmail.com>
2018-05-30 22:50:36 +0200

    Delete old folder

--- 30_Evaulation/Matlab_scripts&funcs/plotsMatlab2017/other/definedAndVariableNumInputs.m
+++
@@ -1,8 +0,0 @@
-function definedAndVariableNumInputs(X,Y,varargin)
-    disp("Total number of input arguments: " + nargin)
-    varargin(1)
-    formatSpec = "Size of varargin cell array: %dx%d";
-    str = compose(formatSpec,size(varargin));
-    disp(str)
-
-end
--- 30_Evaulation/Matlab_scripts&funcs/plotsMatlab2017/other/drawarrow_example.m
+++
@@ -1,6 +0,0 @@
-[x,y] = meshgrid(0:0.2:2,0:0.2:2);
-u = cos(x).*y;
-v = sin(x).*y;
-
-figure
-quiver(x,y,1,1)
